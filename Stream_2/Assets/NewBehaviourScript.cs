0f008e852f9ef6ac8cc41e6d8bb298a563f70900
Dmitriy Mikhaylik <sblazze@gmail.com>
2017-07-12 10:28:44 +0300

    first commit

---
+++ Stream_2/Assets/NewBehaviourScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    public Vector3 mapSize;
+    public GameObject cube;
+    public Material[] materials;
+
+
+
+	// Use this for initialization
+	void Start () {
+		for(int x = 0; x < mapSize.x; x++)
+        {
+            for (int z = 0; z < mapSize.z; z++)
+            {
+                float noice = Mathf.PerlinNoise(x/mapSize.x, z/mapSize.z);
+                int y = Mathf.RoundToInt(noice * mapSize.y);
+                GameObject obj = Instantiate<GameObject>(cube);
+                obj.transform.position = new Vector3(x, y, z);
+                obj.GetComponent<Renderer>().material = 
+                    (y < 6)? materials[0] :
+                    (y < 7) ? materials[1] :
+                    (y < 9) ? materials[2] :
+                    materials[3];
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
